710eb717acbf8ac0b78c7637e3aefe92f1c21d9c
nurd3v <ukimetov.nurtegin@gmail.com>
2026-04-27 17:26:32 +0500

    variables

---
+++ javascript.js
@@ -0,0 +1,15 @@
+// document.write("Hello world"); - это обычный вывод информаций
+// console.log("Hello world"); - это тоже вывод информаций но только в консоль
+// console.error("Hello world"); - это тоже вывод информаций в консоль но в формате ошибки
+// console.warn("Hello world"); - это тоже вывод информаций в консоле но в формате предупреждение
+
+//var num=6; // var - это переменная, num - это название переменной
+//console.log(num); 
+
+/* const num_1 = 7; -- получается const это тоже переменная но мы не можем менять ее значие
+   console.log(num_1);
+*/
+
+var number1 = 5;
+var number2 = "5";
+console.log(number1 + number2);
